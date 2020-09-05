1af8554632ffe5e6e039b55331a5e8ffbf497472
Marcos Lacerda <marcos.lacerda@turbi.com.br>
2020-09-05 14:51:39 -0300

    Consumo da API e apresentação da cotação.

--- Iti/DetailInvestiment/Controller/DetailInvestimentViewController.swift
+++ Iti/DetailInvestiment/Controller/DetailInvestimentViewController.swift
@@ -21,6 +21,7 @@ class DetailInvestimentViewController: UIViewController {
     @IBOutlet weak var lbTotalValue                 : UILabel!
     @IBOutlet weak var lbDate                       : UILabel!
     @IBOutlet weak var lbTotalValueText             : UILabel!
+    @IBOutlet weak var viewLine                     : UIView!
     @IBOutlet weak var lbTodayQuote                 : UILabel!
     @IBOutlet weak var lbTodayValue                 : UILabel!
     @IBOutlet weak var lbTodayQuoteText             : UILabel!
@@ -38,14 +39,58 @@ class DetailInvestimentViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        
+        callExternalAPI(with: "itsa4")
+        
+    }
     
     // MARK: - Actions
    
     @IBAction func btnEdit(_ sender: Any, forEvent event: UIEvent) {
     
+        
     }
     
     @IBAction func btnExit(_ sender: Any, forEvent event: UIEvent) {
     
+        self.dismiss(animated: true, completion: nil)
+    }
+}
+
+extension DetailInvestimentViewController {
+    
+    fileprivate func callExternalAPI(with symbol : String){
+        
+        ForexAPI.loadAction(withSymbol: symbol)
+        { (result) in
+            
+            switch result {
+                
+                case .success(let forexQuote) : self.displayQuoteDetails(quote: forexQuote.quote)
+                
+                case .failure(let error)  : self.handleError(error: error)
+            }
+        }
+    }
+}
+
+
+extension DetailInvestimentViewController {
+    
+    fileprivate func displayQuoteDetails(quote : Forex){
+        
+        DispatchQueue.main.sync {
+            
+            self.lbTodayQuoteText.text = quote.priceFormatted;
+        }
+        
+    }
+}
+
+extension DetailInvestimentViewController {
+    
+    fileprivate func handleError(error : APIError){
+        
     }
 }
