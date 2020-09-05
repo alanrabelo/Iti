f9e976a1a410ace0ac5009a599618be8875cea13
Matheus <matheus.ramos@trigg.com.br>
2020-09-05 15:13:37 -0300

    remocao da linha abaixo da navigation controller

--- Iti/Home/Controller/ViewController.swift
+++ Iti/Home/Controller/ViewController.swift
@@ -12,11 +12,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setupUI()
+    }
+    
+    private func setupUI() {
+        
         let logo = UIImage(named: "logoiti")
         let imageView = UIImageView(image: logo)
         imageView.contentMode = .scaleAspectFit
         self.navigationItem.titleView = imageView
+        
+        navigationController?.navigationBar.shadowImage = UIImage()
     }
 
+    @IBAction func saibaTudo(_ sender: Any) {
+        if let url = URL(string: "https://iti.itau") {
+            UIApplication.shared.open(url)
+        }
+    }
 }
 
