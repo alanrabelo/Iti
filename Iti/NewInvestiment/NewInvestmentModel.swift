1cdeadf718d1d42c44610c8dd3eb66e65a095cf6
Alan Rabelo Martins <alanrabelo13@gmail.com>
2020-09-05 14:18:23 -0300

    - Adding Date extensions - Adding InvestmentModel to NewInvestmentViewController

--- Iti/NewInvestiment/NewInvestmentModel.swift
+++ Iti/NewInvestiment/NewInvestmentModel.swift
@@ -4,7 +4,13 @@ struct NewInvestmentModel {
     
     var active: String
     var quantity: Double
-    var price: Double
+    var price: Int
     var startDate: String
     
+    init(withModel model: Investment?) {
+        self.active = model?.active ?? ""
+        self.startDate = model?.startDate ?? Date().asString
+        self.quantity = model?.quantity ?? 0
+        self.price = Int((model?.price ?? 0) * 100)
+    }
 }
