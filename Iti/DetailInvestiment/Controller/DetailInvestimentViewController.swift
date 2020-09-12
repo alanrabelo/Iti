26e5d4bdb008716bd8dd1a301b1698ece308c985
Italus <italusprado@acad.ifce.edu.br>
2020-09-12 15:45:06 -0300

    inserting viewmodel info

--- Iti/DetailInvestiment/Controller/DetailInvestimentViewController.swift
+++ Iti/DetailInvestiment/Controller/DetailInvestimentViewController.swift
@@ -10,26 +10,40 @@ import UIKit
 
 typealias HomeEnabled = Coordinator & ListInvestmentPresenter
 
-
-
-
 class DetailInvestimentViewController: UIViewController, HasCodeView {
-    
+
     typealias CustomView = DetailInvestimentView
     weak var coordinator: DetailInvestmentCoordinator?
+    
     // MARK: - Properties
     var viewModel: DetailInvestmentViewModel?
-    
+
 
     // MARK: - View Life Cycle
-    
+
     override func loadView() {
-         
-        view = DetailInvestimentView()
+
+        let detailView = DetailInvestimentView()
+        view = detailView
+//        view.lbStockIdentifier.text = viewModel?.active
+
+        detailView.lbQuantityDescription.text = viewModel?.quantity
+
+        detailView.lbPrice.text = viewModel?.price
+
+        detailView.lbDate.text = viewModel?.startDate
+
+//        detailView.lbTotalValueText.text  = initialValue.formattedPrice
+//
+//        detailView.lbTodayQuoteText.text  = quote.priceFormatted;
+//
+//        detailView.lbTodayValueText.text  = currentValue.formattedPrice
+//
+//        detailView.lbProfitability.text   = "\(Int(profitability))%"
     }
 
     override func viewDidLoad() {
-     
+
         super.viewDidLoad()
     }
 
@@ -40,22 +54,22 @@ class DetailInvestimentViewController: UIViewController, HasCodeView {
             let unwrappedSymbol = unwrappedInvestiment.active
 
             viewModel?.callExternalAPI(with: unwrappedSymbol)  { (result) in
-                
+
                 switch result {
-                    
+
                 case .success(let forexQuote) : self.displayQuoteDetails(quote: forexQuote.quote)
-                    
+
                 case .failure(let error)  : self.handleError(error: error)
                 }
             }
-            
+
         }
     }
-    
+
     private func goToHome() {
         coordinator?.showHome()
     }
-    
+
     deinit {
         coordinator?.childDidFinish(nil)
         print("DetailInvestimentViewController deinit")
@@ -64,33 +78,39 @@ class DetailInvestimentViewController: UIViewController, HasCodeView {
 
 extension DetailInvestimentViewController {
 
+
+}
+
+
+extension DetailInvestimentViewController {
+
     fileprivate func displayQuoteDetails(quote : Forex){
 
-       if let unwrappedInvestiment = self.viewModel {
+        if let unwrappedInvestiment = self.viewModel {
 
             DispatchQueue.main.sync {
 
-                let initialValue = (unwrappedInvestiment.quantity * unwrappedInvestiment.price)
-                
-                let currentValue = (unwrappedInvestiment.quantity * (Double(quote.price) ?? 0))
-                
-                let profitability = (currentValue - initialValue)/initialValue*100
-                
-//                self.lbStockIdentifier.text = quote.symbol
+//                let initialValue = (unwrappedInvestiment.quantity * unwrappedInvestiment.price)
 //
-//                self.lbQuantityDescription.text          = String(unwrappedInvestiment.quantity)
+//                let currentValue = (unwrappedInvestiment.quantity * (Double(quote.price) ?? 0))
 //
-//                self.lbPrice.text           = unwrappedInvestiment.price.formattedPrice
+//                let profitability = (currentValue - initialValue)/initialValue*100
 //
-//                self.lbDate.text            = formatDate(with: unwrappedInvestiment.startDate ?? "")
+//                                self.lbStockIdentifier.text = quote.symbol
 //
-//                self.lbTotalValueText.text  = initialValue.formattedPrice
+//                                self.lbQuantityDescription.text          = String(unwrappedInvestiment.quantity)
 //
-//                self.lbTodayQuoteText.text  = quote.priceFormatted;
+//                                self.lbPrice.text           = unwrappedInvestiment.price.formattedPrice
 //
-//                self.lbTodayValueText.text  = currentValue.formattedPrice
+//                                self.lbDate.text            = formatDate(with: unwrappedInvestiment.startDate ?? "")
 //
-//                self.lbProfitability.text   = "\(Int(profitability))%"
+//                                self.lbTotalValueText.text  = initialValue.formattedPrice
+//
+//                                self.lbTodayQuoteText.text  = quote.priceFormatted;
+//
+//                                self.lbTodayValueText.text  = currentValue.formattedPrice
+//
+//                                self.lbProfitability.text   = "\(Int(profitability))%"
             }
         }
     }
--- Iti/DetailInvestiment/ViewModel/DetailInvestmentViewModel.swift
+++ Iti/DetailInvestiment/ViewModel/DetailInvestmentViewModel.swift
@@ -3,26 +3,26 @@ import Foundation
 class DetailInvestmentViewModel {
     
      private var investment: Investment
-    
-     init(investment: Investment) {
-       self.investment = investment
-     }
-    
-    var quantity: Double {
-        return investment.quantity
-    }
-    
-    var price: Double {
-        return investment.price
-    }
-    
-    var active: String {
-        return investment.active!
-    }
 
-    var startDate: String {
-        return investment.startDate!
-    }
+     init(investment: Investment) {
+         self.investment = investment
+     }
+
+     var quantity: String {
+         return "\(investment.quantity)"
+     }
+
+     var price: String {
+         return investment.price.formattedPrice
+     }
+
+     var active: String {
+         return investment.active!
+     }
+
+     var startDate: String {
+         return investment.startDate!
+     }
     
     func callExternalAPI(with symbol : String, onComplete: @escaping (Result<ForexQuote, APIError>) -> Void){
 
